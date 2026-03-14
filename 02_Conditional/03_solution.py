70585c8ec69c89ae85f40128cde8df50d7a1709a
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-03-14 22:18:30 +0530

    03 conditionals

---
+++ 02_Conditional/03_solution.py
@@ -0,0 +1,17 @@
+score = 20
+
+if score >= 101:
+    print("Please enter a valid grade score")
+    exit()
+
+
+if score >= 90 and score <= 100:
+    print("A")
+elif score >= 80 and score <= 89:
+    print("B")
+elif score >= 70 and score <= 79:
+    print("C")
+elif score >= 60 and score <= 69:
+    print("D")
+elif score < 60:
+    print("F")
