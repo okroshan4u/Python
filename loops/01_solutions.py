0a18324ab9ee9f809889f2775d16b58aa5ea9a05
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-03-15 22:06:02 +0530

    loops in python

---
+++ loops/01_solutions.py
@@ -0,0 +1,10 @@
+# Find the positive number from a given number of list
+
+numbers = [1,-2,3,-8,4,-6,8,7,4,-160,12]
+
+sum = 0;
+for num in numbers:
+    if(num>0):
+        sum = sum + 1;
+        
+print(sum)        
