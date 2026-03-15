4c591033ef43efc9582dffa27454b21fa01af5f4
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-03-15 22:06:37 +0530

    loops 2 in python

---
+++ loops/02_solutions.py
@@ -0,0 +1,14 @@
+# print the number in the range given by user and add the even number amoung them
+
+sum = 0
+
+num = int(input("Enter your num upto which you want the sum of even number: "))
+
+print("The number you entered is : ",num)
+
+
+for i in range(num):
+    print(f"The numbers from 1 to {num} are : ",i+1)
+    if(i+1) % 2 == 0:
+        sum = sum + (i+1)
+print(f"The sum of the even numbers from 1 to {num} is : ",sum)        
