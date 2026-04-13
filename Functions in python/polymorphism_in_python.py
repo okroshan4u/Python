cfca1140dc5762aa251a7fde2fe36938978ea3e9
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-04-13 21:12:13 +0530

    Add files via upload

---
+++ Functions in python/polymorphism_in_python.py
@@ -0,0 +1,16 @@
+# Write a functin named multiply that multiplies tow numbers, but can also accepts and mulitply strings
+
+# def multiply(p1, p2):
+#     return p1 * p2
+
+
+def multiply(p1, p2):
+    if isinstance(p1, str) and isinstance(p2, str): # if p1 is a string AND p2 is also a string then.....
+        return p1 + p2
+    return p1 * p2
+
+
+print(multiply(4,6))
+print(multiply("r",6))
+print(multiply(4,"h"))
+print(multiply("r","h"))
