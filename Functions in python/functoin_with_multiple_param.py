4975ecbd57f5aa9d18aa9d94540f280c834516ba
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-04-14 19:25:34 +0530

    Fix comment formatting and clarify parameter names

--- Functions in python/functoin_with_multiple_param.py
+++ Functions in python/functoin_with_multiple_param.py
@@ -1,6 +1,6 @@
 # Create a function that takes two numbered as paramets and return their sum
 
-def sum_of_two_num( a,  b):
+def sum_of_two_num( a,  b): # here it is called parameters
     return float(a + b )
 
 print(sum_of_two_num(4,5))
