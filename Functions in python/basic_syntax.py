83fb13c0e2bb218aa12f897d2d0de247f24b4c3e
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-04-16 21:45:09 +0530

    Add files via upload

---
+++ Functions in python/basic_syntax.py
@@ -0,0 +1,12 @@
+# def square_of_num(numbers):  ## here numbers we are taking are called the parameters no the arguments 
+#     print(numbers**2)  ## here we are not returning anything so saving this in a result will give none
+
+# square_of_num(4) 
+
+
+
+def square_of_num(numbers):
+   return numbers**2  ## here we are returning  so saving this in a result will give correct result
+
+result = square_of_num(4)
+print(result)    
