10c02306d5c558fe57548991eac15dbb4c66d083
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-04-17 20:59:11 +0530

    Update 02_python_inner.py

--- 02_python_inner.py
+++ 02_python_inner.py
@@ -6,4 +6,4 @@ from hello import tell
 tell("Roshan")
 
 # here __pycashe__ file generated when we import file from other file 
-
+# It is always generated but can only be visible to us when we are importing a file from other 
