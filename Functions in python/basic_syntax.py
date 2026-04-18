887e5e1f1a63573070d40111607025c35a3de04b
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-04-18 19:40:55 +0530

    Update basic_syntax.py

--- Functions in python/basic_syntax.py
+++ Functions in python/basic_syntax.py
@@ -1,7 +1,7 @@
 # def square_of_num(numbers):  ## here numbers we are taking are called the parameters no the arguments 
 #     print(numbers**2)  ## here we are not returning anything so saving this in a result will give none
 
-# square_of_num(4) #
+# square_of_num(4) 
 
 
 
