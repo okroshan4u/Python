7abf172b763b8d734a174457560003e8c1b6a6fe
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-04-27 14:24:19 +0530

    Fix typo in function comment and improve formatting

--- Functions in python/functoin_with_multiple_param.py
+++ Functions in python/functoin_with_multiple_param.py
@@ -1,4 +1,4 @@
-# Create a function that takes two numbered as paramets and return their su
+# Create a function that takes two numbered as paramets and return their sum
 
 def sum_of_two_num( a,  b): # here it is called parameters
     return float(a + b )
