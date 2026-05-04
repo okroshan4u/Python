560e4cf6ad10408b09615187fa55fa07ed20a69d
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-05-04 20:45:46 +0530

    Update 02_python_inner.py

--- 02_python_inner.py
+++ 02_python_inner.py
@@ -1,6 +1,6 @@
 # what are the files comes in or generated when we import the file and use that into other file 
 # __pycashe__ file generated
-from hello import tel
+from hello import te
 
 
 tell("Roshan")
