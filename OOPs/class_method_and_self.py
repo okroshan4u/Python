51ce02d94e7e47130424578a10b76e01378df668
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-06-14 07:44:18 -0700

    Add files via upload

---
+++ OOPs/class_method_and_self.py
@@ -0,0 +1,15 @@
+#Add a method to the Car class that displays the full name of the car (brand and model)
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand 
+        self.model = model
+      
+    def fullname(self):
+        return f"{self.brand} {self.model}"
+      
+my_car = Car("Toyota","Corolla")
+
+print(my_car.brand)        
+print(my_car.model)        
+print(my_car.fullname())        
