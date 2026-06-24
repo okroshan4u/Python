de10d3e1cf309258b4b701b90ec3248effd9a63b
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-06-24 21:12:29 +0530

    Refactor Car class to use property decorator for model

--- OOPs/Property_Decorators.py
+++ OOPs/Property_Decorators.py
@@ -1,4 +1,4 @@
-# Use a property decorator in the Car class to make the model attribute read-on
+# Use a property decorator in the Car class to make the model attribute read-only
 
 class Car:
     def __init__(self, brand, model):
