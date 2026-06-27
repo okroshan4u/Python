84b2e6b6bba3fd440c78389f084359293c6aa3f5
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-06-27 22:16:15 +0530

    Fix formatting issues in basic_class_and_obj.py

--- OOPs/basic_class_and_obj.py
+++ OOPs/basic_class_and_obj.py
@@ -1,4 +1,4 @@
-# Create a Car class with attributes like brand and model. Then create an instance of this cal
+# Create a Car class with attributes like brand and model. Then create an instance of this ca
 
 class Car:
     def __init__(self, brand, model):
